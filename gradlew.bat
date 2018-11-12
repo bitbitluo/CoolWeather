5d5b0295d4d710714b7846a99f67e2d5c1aabba6
bitbitluo <bitbitluo@outlook.com>
2018-11-12 21:56:42 +0800

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,49 @@
+<application>
+  <component name="RecentProjectsManager">
+    <option name="recentPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/program" />
+        <option value="$USER_HOME$/Desktop/RandMoney" />
+        <option value="$USER_HOME$/Desktop/GoBang" />
+        <option value="$USER_HOME$/Desktop/Test" />
+        <option value="$USER_HOME$/Desktop/MyApplication4" />
+        <option value="$USER_HOME$/Desktop/MyApplication3" />
+        <option value="$USER_HOME$/Desktop/MyApplication2" />
+        <option value="$USER_HOME$/Desktop/MyApplication" />
+        <option value="$USER_HOME$/Documents/OpenCV" />
+        <option value="$USER_HOME$/Desktop/OpenCV-android-sdk" />
+        <option value="$USER_HOME$/AndroidStudioProjects/openCV" />
+        <option value="$USER_HOME$/AndroidStudioProjects/RookieBlog" />
+        <option value="$USER_HOME$/AndroidStudioProjects/Test" />
+        <option value="$USER_HOME$/AndroidStudioProjects/ActivityLife" />
+        <option value="$USER_HOME$/AndroidStudioProjects/shop" />
+      </list>
+    </option>
+    <option name="openPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/RandMoney" />
+        <option value="$USER_HOME$/Desktop/program" />
+      </list>
+    </option>
+    <option name="lastPath" value="$USER_HOME$/Desktop/program" />
+    <option name="pid" value="" />
+    <option name="additionalInfo">
+      <map>
+        <entry key="$USER_HOME$/AndroidStudioProjects/ActivityLife">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-173.4720617" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1538396337596" />
+              <option name="buildTimestamp" value="1523654860516" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="$USER_HOME$/AndroidStudioProjects/RookieBlog">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-173.4720617" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+            
